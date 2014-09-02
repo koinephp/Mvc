407da042c078343620b16de5aa52bd7522871756
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-09-02 11:24:18 -0300

    Implemented controller

--- lib/Koine/Mvc/Controller.php
+++ lib/Koine/Mvc/Controller.php
@@ -2,10 +2,124 @@
 
 namespace Koine\Mvc;
 
+use Koine\Object;
+use Koine\Http\Request;
+use Koine\Http\Response;
+
 /**
  * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
  */
-class Controller
+class Controller extends Object
 {
 
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var View
+     */
+    protected $view;
+
+    /**
+     * Method run before the real action
+     */
+    public function beforeAction()
+    {
+
+    }
+
+    /**
+     * Method run after the real action
+     */
+    public function afterAction()
+    {
+
+    }
+
+    /**
+     * Sets the response object
+     *
+     * @param  Response $response
+     * @return self
+     */
+    public function setResponse(Response $response)
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * Get the response object
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
+    /**
+     * Get the request
+     *
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Get the request
+     *
+     * @param  View $view
+     * @return set
+     */
+    public function setView(View $view)
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+    /**
+     * Get the view
+     *
+     * @return View
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * Appends template content to the body
+     *
+     * @param string $template
+     * @param array  $localVariables
+     */
+    public function render($template, array $localVariables = array())
+    {
+        $content = $this->getView()->render($template, $localVariables);
+        $this->getResponse()->setBody($content);
+
+        return $this;
+    }
 }
--- tests/KoineTests/Mvc/ControllerTest.php
+++ tests/KoineTests/Mvc/ControllerTest.php
@@ -2,8 +2,15 @@
 
 namespace KonieTests\Mvc;
 
-use Koine\Mvc\Controller;
 use PHPUnit_Framework_TestCase;
+use Koine\Mvc\Controller;
+use Koine\Mvc\View;
+use Koine\Http\Request;
+use Koine\Http\Response;
+use Koine\Http\Session;
+use Koine\Http\Cookies;
+use Koine\Http\Params;
+use Koine\Http\Environment;
 
 /**
  * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
@@ -20,8 +27,69 @@ class ControllerTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function canBeInstantiated()
+    public function extendsKoineHash()
     {
-        $this->assertInstanceOf('Koine\Mvc\Controller', $this->object);
+        $this->assertInstanceOf('Koine\Object', $this->object);
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndGetResponse()
+    {
+        $expected = new Response();
+        $response = $this->object->setResponse($expected)->getResponse();
+        $this->assertSame($expected, $response);
+    }
+
+    /**
+     * @test
+     */
+    public function canSetAndGetRequest()
+    {
+        $params = array();
+
+        $expected = new Request(array(
+            'session'     => new Session($params),
+            'environment' => new Environment($params),
+            'cookies'     => new Cookies($params),
+            'params'      => new Params($params),
+        ));
+
+        $request = $this->object->setRequest($expected)->getRequest();
+
+        $this->assertSame($expected, $request);
+    }
+
+    /**
+     * @test
+     */
+    public function canGetAndSetView()
+    {
+        $expected = new View();
+        $view = $this->object->setView($expected)->getView();
+        $this->assertSame($expected, $view);
+    }
+
+    /**
+     * @test
+     */
+    public function renderSetTheContentToTheResponse()
+    {
+        $response = new Response();
+
+        $view = $this->getMock('Koine\Mvc\View');
+        $view->expects($this->once())
+            ->method('render')
+            ->with('foo', array('foo' => 'bar'))
+            ->will($this->returnValue('hello world'));
+
+        $this->object
+            ->setResponse($response)
+            ->setView($view);
+
+        $this->object->render('foo', array('foo' => 'bar'));
+
+        $this->assertEquals('hello world', $response->getBody());
     }
 }
